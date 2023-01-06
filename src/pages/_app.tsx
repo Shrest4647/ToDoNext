b816fcf3360911ec1993447dcf479ddba67c0db0
Sunil Shrestha <shrest464760+github@gmail.com>
2023-01-06 22:33:33 +0545

    Applied the components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import { AppProps } from 'next/app';
+import { useState } from 'react';
 
 import '@/styles/globals.css';
-// !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
 import '@/styles/colors.css';
+
+import { TodoProvider } from '@/store/TodoProvider';
+
+import { Todo } from '@/types/Todo';
 
 /**
  * !STARTERCONF info
@@ -10,7 +14,40 @@ import '@/styles/colors.css';
  */
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const addNewTodo = (taskName: string) => {
+    setTodos([
+      ...todos,
+      {
+        taskName,
+        isComplete: false,
+      },
+    ]);
+  };
+  const deleteTodo = (index: number) => {
+    if (index < todos.length) {
+      setTodos([...todos.filter((_, indx) => indx !== index)]);
+    }
+  };
+  const updateTodo = (newTodo: Todo, index: number) => {
+    if (index < todos.length) {
+      setTodos([...todos.splice(index, 1, newTodo)]);
+    }
+  };
+  const toggleCompleteTodo = (index: number) => {
+    if (index < todos.length) {
+      const newlist = todos;
+      newlist[index].isComplete = !newlist[index].isComplete;
+      setTodos([...newlist]);
+    }
+  };
+  return (
+    <TodoProvider.Provider
+      value={{ todos, addNewTodo, deleteTodo, updateTodo, toggleCompleteTodo }}
+    >
+      <Component {...pageProps} />
+    </TodoProvider.Provider>
+  );
 }
 
 export default MyApp;
